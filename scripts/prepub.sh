f20b8221d9d670cab9ac44f7bb7f35e574870298
liyubei <liyubei@baidu.com>
2018-01-29 15:05:38 +0800

    Release 0.1.4
    Change-Id: I1e5f6f2fad44a17c151a4f60219dbeeb94f3ef70

--- scripts/prepub.sh
+++ scripts/prepub.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-rm -rf lib dist
+rm -rf lib dist es
 
 babel src --presets babel-preset-es2015 --out-dir lib
+
+cp -r src es
+find es -name '*.es6' -exec sh -c 'mv "$0" "${0%.es6}.js"' {} \;
 
 mkdir -p dist
 lessc -ru src/x/xui.less > dist/xui.css
